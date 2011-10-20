e5740cbab3311cbbe3548e6acd63ae71033cda59
Matthew Schinckel <matt@schinckel.net>
2011-10-20 13:09:55 +1030

    Updated readme and version to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = "django-timedeltafield",
-    version = "0.4.7",
+    version = "0.5",
     description = "TimedeltaField for django models",
     url = "http://hg.schinckel.net/django-timedelta-field/",
     author = "Matthew Schinckel",
