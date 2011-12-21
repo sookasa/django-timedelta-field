73626113fe19718a5ef4c5f5b0d631bdb3fc2c94
Matthew Schinckel <matt@schinckel.net>
2011-12-21 17:24:56 +1030

    Don't fail installing if django is not yet installed.

--- timedelta/__init__.py
+++ timedelta/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "0.5.1"
 
-from fields import TimedeltaField
-
-from helpers import divide, multiply, modulo, parse, nice_repr, percentage
+try:
+    from fields import TimedeltaField
+    from helpers import divide, multiply, modulo, parse, nice_repr, percentage
+except ImportError:
+    pass
