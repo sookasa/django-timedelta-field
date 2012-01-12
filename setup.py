b0dbe859ee0e00c06b7b4ed895c17520707f5ad4
Matthew Schinckel <matt@schinckel.net>
2012-01-12 10:59:41 +1030

    Include long_description from README in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name = "django-timedeltafield",
     version = __version__,
     description = "TimedeltaField for django models",
+    long_description = open("README").read(),
     url = "http://hg.schinckel.net/django-timedelta-field/",
     author = "Matthew Schinckel",
     author_email = "matt@schinckel.net",
