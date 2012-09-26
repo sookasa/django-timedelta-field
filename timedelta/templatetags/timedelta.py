f85a7797950eaef553c27c71494c698f5f32a0dc
Matthew Schinckel <matt@schinckel.net>
2012-09-26 17:13:19 +0930

    Added new template filters for number of seconds.

--- timedelta/templatetags/timedelta.py
+++ timedelta/templatetags/timedelta.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 
 # Don't really like using relative imports, but no choice here!
-from ..helpers import nice_repr, iso8601_repr
+from ..helpers import nice_repr, iso8601_repr, total_seconds as _total_seconds
 
 @register.filter(name='timedelta')
 def timedelta(value, display="long"):
@@ -11,3 +11,12 @@ def timedelta(value, display="long"):
 @register.filter(name='iso8601')
 def iso8601(value):
     return iso8601_repr(value)
+
+@register.filter(name='total_seconds')
+def total_seconds(value):
+    return _total_seconds(value)
+
+@register.filter(name='total_seconds_sort')
+def total_seconds(value, places=10):
+    return ("%0" + str(places) + "i") % _total_seconds(value)
+
