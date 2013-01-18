1f0f9202fd64f24e070384945bacc2d2639b5ff7
Matthew Schinckel <matt@schinckel.net>
2013-01-18 15:52:35 +1030

    Possible fix for pull-request #6.

--- timedelta/__init__.py
+++ timedelta/__init__.py
@@ -3,12 +3,17 @@ import os
 __version__ = open(os.path.join(os.path.dirname(__file__), "VERSION")).read().strip()
 
 try:
-    from fields import TimedeltaField
-    from helpers import (
-        divide, multiply, modulo, 
-        parse, nice_repr, 
-        percentage, decimal_percentage,
-        total_seconds
-    )
+    from django.core.exceptions import ImproperlyConfigured
 except ImportError:
     pass
+else:
+    try:
+        from fields import TimedeltaField
+        from helpers import (
+            divide, multiply, modulo, 
+            parse, nice_repr, 
+            percentage, decimal_percentage,
+            total_seconds
+        )
+    except (ImportError, ImproperlyConfigured):
+        pass
